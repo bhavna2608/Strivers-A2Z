055c1939e95e6be77a9172b3ca19b6d924f37224
Bhavna Jayswal <139497517+bhavna2608@users.noreply.github.com>
2024-03-01 23:02:12 +0530

    Update Solution.py

--- The-Basics/User Input-Output/Solution.py
+++ The-Basics/User Input-Output/Solution.py
@@ -1,5 +1,5 @@
-a = input()
-b = ord(a)
+a = input() #that is how you take input in python; note that the data type of the input is always treated as a string here
+b = ord(a) #The ord() function returns a number representing the unicode (ASCII) code of a specified character
 if b > 64 and b < 91:
     print("1")
 elif b > 96 and b < 123:
