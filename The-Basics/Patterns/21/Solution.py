00919f2ca80e45aa151749f751d401e63f507fce
Bhavna Jayswal <139497517+bhavna2608@users.noreply.github.com>
2024-03-03 17:24:21 +0530

    Create Solution.py

---
+++ The-Basics/Patterns/21/Solution.py
@@ -0,0 +1,10 @@
+def getStarPattern(n: int) -> None:
+    # Write your solution here.
+    for i in range(n):
+        if i == 0 or i == n-1:
+            print("*"*n)
+        else:
+            print("*", end="")
+            print(" "*(n-2), end="")
+            print("*")
+    pass
